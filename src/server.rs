51789195b3e6d4190192c20da6c09f389d9e543e
WKHAllen <wkhallen@gmail.com>
2020-09-18 11:41:18 -0400

    Implement server serve method

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 pub mod server {
 	use std::net::{TcpListener, TcpStream, Shutdown};
+	use std::thread;
+	use std::io;
+	use std::time::Duration;
 
 	enum ServerSock {
 		Sock(TcpListener),
@@ -20,7 +23,6 @@ pub mod server {
 		serving: bool,
 		sock: ServerSock,
 		clients: Vec<ClientSock>,
-		next_client_id: usize,
 		// TODO: add other attributes
 	}
 
@@ -41,7 +43,6 @@ pub mod server {
 				serving: false,
 				sock: ServerSock::Null,
 				clients: Vec::new(),
-				next_client_id: 0,
 			}
 		}
 
@@ -53,7 +54,7 @@ pub mod server {
 			Server::new(on_recv, on_connect, on_disconnect, false, false)
 		}
 
-		pub fn start(&mut self, host: &str, port: u16) -> std::io::Result<()> {
+		pub fn start(&mut self, host: &str, port: u16) -> io::Result<()> {
 			if !self.serving {
 				let addr = format!("{}:{}", host, port);
 				let listener = TcpListener::bind(addr)?;
@@ -61,7 +62,7 @@ pub mod server {
 
 				self.serving = true;
 				if self.blocking {
-					// self.serve();
+					self.serve()?;
 				} else {
 					// TODO: spawn serve method in thread
 				}
@@ -70,19 +71,19 @@ pub mod server {
 			Ok(())
 		}
 
-		pub fn start_default_host(&mut self, port: u16) -> std::io::Result<()> {
+		pub fn start_default_host(&mut self, port: u16) -> io::Result<()> {
 			self.start("0.0.0.0", port)
 		}
 
-		pub fn start_default_port(&mut self, host: &str) -> std::io::Result<()> {
+		pub fn start_default_port(&mut self, host: &str) -> io::Result<()> {
 			self.start(host, 0)
 		}
 
-		pub fn start_default(&mut self) -> std::io::Result<()> {
+		pub fn start_default(&mut self) -> io::Result<()> {
 			self.start("0.0.0.0", 0)
 		}
 
-		pub fn stop(&mut self) -> std::io::Result<()> {
+		pub fn stop(&mut self) -> io::Result<()> {
 			if self.serving {
 				self.serving = false;
 
@@ -92,11 +93,59 @@ pub mod server {
 						ClientSock::Null => Ok(()),
 					}?;
 				}
-
-				// TODO: shut down listener
 			}
 
 			Ok(())
+		}
+
+		fn serve(&mut self) -> io::Result<()> {
+			match &self.sock {
+				ServerSock::Sock(listener) => {
+					listener.set_nonblocking(true)?;
+					for stream in listener.incoming() {
+						let result = match stream {
+							Ok(conn) => {
+								let client_id = self.clients.len();
+								self.exchange_keys(client_id, &conn)?;
+								self.clients.push(ClientSock::Sock(conn));
+								self.serve_client(client_id); // TODO: spawn new thread for this
+								Ok(())
+							},
+							Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+								if self.serving {
+									Ok(())
+								} else {
+									Err(io::Error::new(io::ErrorKind::Other, "Done"))
+								}
+							},
+							Err(e) => Err(e),
+						};
+
+						if result.is_err() {
+							let result_err = result.err().unwrap();
+							if result_err.kind() == io::ErrorKind::Other {
+								return Ok(());
+							} else {
+								return Err(result_err);
+							}
+						}
+
+						thread::sleep(Duration::from_millis(10));
+					}
+
+					Ok(()) // unreachable; to satisfy the compiler
+				},
+				ServerSock::Null => Ok(()),
+			}
+		}
+
+		fn exchange_keys(&self, client_id: usize, conn: &TcpStream) -> io::Result<()> {
+			// TODO: implement key exchange
+			Ok(())
+		}
+
+		fn serve_client(&self, client_id: usize) {
+			// TODO: implement client serving
 		}
 
 		// TODO: complete implementation
